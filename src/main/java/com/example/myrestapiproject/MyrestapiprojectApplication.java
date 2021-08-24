b509593e6cb12531739b51545ed33c78d8bf44f8
Jaiganesh Shanmugam <jaig.shan@gmail.com>
2021-08-24 12:33:47 -0400

    First Commit

---
+++ src/main/java/com/example/myrestapiproject/MyrestapiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrestapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestapiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrestapiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myrestapiproject/MyrestapiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrestapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestapiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
